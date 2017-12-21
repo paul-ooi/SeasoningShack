448274c23b551018b01becac02cdffaf806b91c5
Paul O <info@paulooidesign.com>
2017-12-21 00:10:56 -0500

    created separate header and footer HTML, uploaded images for Homepage

--- JavaScript/header-footer.js
+++ JavaScript/header-footer.js
@@ -1,30 +1,25 @@
 window.addEventListener("load", pageReady, false);
 
 function pageReady() {
-
   //Make Header change size on Scroll
+  var fullpage = window.document;
   var header = document.getElementsByTagName('header')[0];
-var mainElem = document.getElementById('locations');
-console.log(document.scrollTop);
+  var mainElem = document.getElementById('locations');
+
+
   function resizeHeader(){
-      console.log(window.scrollTop)
-      if (document.scrollTop < 500){
+      if (window.pageYOffset < 400){
         console.log('first fired');
       header.classList.remove('small');
       header.classList.add('large');
-      } else {
+      }
+      if (window.pageYOffset > 400){
         console.log('2nd fired');
       header.classList.remove('large');
       header.classList.add('small');
       }
-  }
+  }//End of resizeHeader
 
-
-  mainElem.addEventListener("wheel",resizeHeader,false);
-
-
-
-
-
+  fullpage.addEventListener("scroll",resizeHeader,false);//Listen for scrolled movement
 
 }//end of pageReady
