a0efa96896862f3a3f2cc5ff5b32e083a7ff4dc1
Czarina <czarinagahunia@gmail.com>
2018-01-09 07:28:30 -0500

    Updated gift cards page

--- JavaScript/gift-cards.js
+++ JavaScript/gift-cards.js
@@ -6,12 +6,16 @@ function pageReady() {
   var giftC100 = document.getElementById("gc-100");
   var gift50 = document.getElementById("50gc");
   var gift100 = document.getElementById("100gc");
+  var giftCardBox = document.getElementById("gift-card-content");
   
   var buyerForm = document.getElementById("buyer-details");
   
+  var subForm = document.forms.gift_card_name;
+  var confirmCard = document.getElementById("confirmation");
+  
   giftC100.onclick = card100;
   giftC50.onclick = card50;
-  
+
   function fadeIn(buyerForm) {
     buyerForm.style.opacity = 0;
 
@@ -23,16 +27,15 @@ function pageReady() {
       }
     };
     clickCard();
-    }
-
+  }
+  
   function card100() {
     if (giftC100.onclick) {
       gift100.style.opacity = "1";
       gift50.style.opacity = null;
       fadeIn(buyerForm);
       buyerForm.style.display = "block";
-
-    }
+    } 
   }
   function card50() {
     if (giftC50.onclick) {
@@ -40,13 +43,14 @@ function pageReady() {
       gift100.style.opacity = null;
       fadeIn(buyerForm);
       buyerForm.style.display = "block";
-    }
+    } 
   }
-  
-  
-  
-  
-  
-  
-  
+
+  subForm.onsubmit = function () {
+    scrollTo(document.body, 0);
+    giftCardBox.style.display = "none";
+    confirmCard.style.display = "block";
+    return false;
+  }
+
 }
