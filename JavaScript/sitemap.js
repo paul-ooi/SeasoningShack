30cfe05594acba643bf09ce2effd4045ec07a975
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-01-09 17:13:49 -0500

    fixed sitemap spacing

--- JavaScript/sitemap.js
+++ JavaScript/sitemap.js
@@ -3,34 +3,26 @@ window.addEventListener('load',pageReady,false);
 function pageReady() {
 var menuLink = document.getElementById("menu-link");
 var specialsLink = document.getElementById("specials-link");
-var childLinks = document.getElementsByClassName("hidden-links");
+var childLinks = document.getElementsByClassName("sub-link-list");
 
 /*show hide links for menu*/
 menuLink.onmouseover = showMenu;
 menuLink.onmouseout = hideMenu;
 function showMenu() {
-	for (var i = 0; i < 5; i++) {
-		childLinks[i].style.display = "inline-block";
-	}
+	childLinks[0].style.display = "block";
 }
 function hideMenu() {
-	for (var i = 0; i < 5; i++) {
-		childLinks[i].style.display = "none";
-	}
+	childLinks[0].style.display = "none";
 }
 
 /*show hide links for specials*/
 specialsLink.onmouseover = showSpecials;
 specialsLink.onmouseout = hideSpecials;
 function showSpecials() {
-	for (var i = 5; i < 8; i++) {
-		childLinks[i].style.display = "inline-block";
-	}
+	childLinks[1].style.display = "block";
 }
 function hideSpecials() {
-	for (var i = 5; i < 8; i++) {
-		childLinks[i].style.display = "none";
-	}
+	childLinks[1].style.display = "none";
 }
 
 
