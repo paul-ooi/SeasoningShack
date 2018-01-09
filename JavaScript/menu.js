0f778cd131443e1b196de5b0b6034e1f3e501abd
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-01-09 17:15:40 -0500

    fixed white space after footer

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -8,56 +8,56 @@ console.log(contents);
 
 headings[0].onclick = toggle0;
 function toggle0() {
-	if (contents[0].style.position === "absolute") {
+	if (contents[0].style.position == "absolute") {
 		contents[0].style.position = "relative";
-		contents[0].style.left = "0px";
+		contents[0].style.top = "0px";
 	}else{
 		contents[0].style.position = "absolute";
-		contents[0].style.left = "10000px";
+		contents[0].style.top = "-10000px";
 	}
 }
 
 headings[1].onclick = toggle1;
 function toggle1() {
-	if (contents[1].style.position === "relative") {
+	if (contents[1].style.position == "relative") {
 		contents[1].style.position = "absolute";
-		contents[1].style.left = "10000px";
+		contents[1].style.top = "-10000px";
 	}else{
 		contents[1].style.position = "relative";
-		contents[1].style.left = "0px";
+		contents[1].style.top = "0px";
 	}
 	
 }
 headings[2].onclick = toggle2;
 function toggle2() {
-	if (contents[2].style.position === "relative") {
+	if (contents[2].style.position == "relative") {
 		contents[2].style.position = "absolute";
-		contents[2].style.left = "10000px";
+		contents[2].style.top = "-10000px";
 	}else{
 		contents[2].style.position = "relative";
-		contents[2].style.left = "0px";
+		contents[2].style.top = "0px";
 	}
 }	
 
 headings[3].onclick = toggle3;
 function toggle3() {
-	if (contents[3].style.position === "relative") {
+	if (contents[3].style.position == "relative") {
 		contents[3].style.position = "absolute";
-		contents[3].style.left = "10000px";
+		contents[3].style.top = "-10000px";
 	}else{
 		contents[3].style.position = "relative";
-		contents[3].style.left = "0px";
+		contents[3].style.top = "0px";
 	}
 }
 
 headings[4].onclick = toggle4;
 function toggle4() {
-	if (contents[4].style.position === "relative") {
+	if (contents[4].style.position == "relative") {
 		contents[4].style.position = "absolute";
-		contents[4].style.left = "10000px";
+		contents[4].style.top = "-10000px";
 	}else{
 		contents[4].style.position = "relative";
-		contents[4].style.left = "0px";
+		contents[4].style.top = "0px";
 	}
 }
 
