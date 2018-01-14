0779041719be5b835a83b9d7101baafc99b0a1cf
Paul O <info@paulooidesign.com>
2018-01-14 13:30:32 -0500

    updated indentations and removed console messages

--- JavaScript/homepage.js
+++ JavaScript/homepage.js
@@ -1,71 +1,68 @@
 window.addEventListener('load', pageReady, false);
 
 function pageReady() {
-//Grab Slides and Slider Index
-var promoSlides = document.getElementsByClassName('promo-detail');
-// console.log(promoSlides);
-var promoIndexContainer = document.getElementById('feature-index');
-// console.log(promoIndexContainer);
-var promoIndex = promoIndexContainer.getElementsByTagName('li');
-// console.log(promoIndex);
-var currentSlide;
-var currentIndex;
+  //Grab Slides and Slider Index
+  var promoSlides = document.getElementsByClassName('promo-detail');
+  // console.log(promoSlides);
+  var promoIndexContainer = document.getElementById('feature-index');
+  // console.log(promoIndexContainer);
+  var promoIndex = promoIndexContainer.getElementsByTagName('li');
+  // console.log(promoIndex);
+  var currentSlide;
+  var currentIndex;
 
 
-//Setup the Interval and counter variables
-var runInterval;
-var counter = 0;
+  //Setup the Interval and counter variables
+  var runInterval;
+  var counter = 0;
 
-function changeSlide() {
-  counter++; //advance counter to be used to target the next sibling
-  //If the counter goes past the max number of slides, it goes back to the start
-  if (counter >= promoSlides.length) {
-  counter = 0;
+  function changeSlide() {
+    counter++; //advance counter to be used to target the next sibling
+    //If the counter goes past the max number of slides, it goes back to the start
+    if (counter >= promoSlides.length) {
+    counter = 0;
+    }
+    deactivateCurrent();
+    activateNext();
   }
-  deactivateCurrent();
-  activateNext();
-}
 
-//Function interupts the automated slides to go to selected index
-function changeNow(selected) {
-  clearInterval(runInterval);
-  deactivateCurrent();
-  counter = selected.value;
-  activateNext();
-  //reactivate automated slides
+  //Function interupts the automated slides to go to selected index
+  function changeNow(selected) {
+    clearInterval(runInterval);
+    deactivateCurrent();
+    counter = selected.value;
+    activateNext();
+    //reactivate automated slides
+    runInterval = setInterval(changeSlide,2500);
+  }
+
+  //Hide the current Slide
+  function deactivateCurrent() {
+    currentIndex = document.querySelector('.current');
+    currentSlide = document.querySelector('.active');
+    currentSlide.classList.remove('active');
+    currentIndex.classList.remove('current');
+  }
+
+  //Show the next Slide
+  function activateNext() {
+    promoSlides[counter].classList.add('active');
+    promoIndex[counter].classList.add('current');
+  }
+
+  //Helper function to add Listener to a List of elements
+  function addListener (listVar, eventString, funcName) {
+    for (var i=0; i < listVar.length; i++) {
+      // console.log(listVar[i]);
+      //Pass the target object to the function called by the eventListener
+      listVar[i].addEventListener(eventString,function(){funcName(this)}, false);
+    }//end of for loop
+  }//end of addListener
+
+  //Begin automated Slide progression
   runInterval = setInterval(changeSlide,2500);
-}
-
-//Hide the current Slide
-function deactivateCurrent() {
-currentIndex = document.querySelector('.current');
-currentSlide = document.querySelector('.active');
-currentSlide.classList.remove('active');
-currentIndex.classList.remove('current');
-}
-
-//Show the next Slide
-function activateNext() {
-  promoSlides[counter].classList.add('active');
-  promoIndex[counter].classList.add('current');
-}
-
-//Helper function to add Listener to a List of elements
-function addListener (listVar, eventString, funcName) {
-  for (var i=0; i < listVar.length; i++) {
-    // console.log(listVar[i]);
-    //Pass the target object to the function called by the eventListener
-    listVar[i].addEventListener(eventString,function(){funcName(this)}, false);
-  }//end of for loop
-}//end of addListener
-
-//Begin automated Slide progression
-runInterval = setInterval(changeSlide,2500);
 
 
-addListener (promoIndex, "click", changeNow);
-
-
-
+  addListener (promoIndex, "click", changeNow);
 
 }//end of pageReady
--- JavaScript/locations.js
+++ JavaScript/locations.js
@@ -1,68 +1,66 @@
 window.addEventListener('load', pageReady, false);
 
 function pageReady() {
-var todayDate = new Date();
-var today = todayDate.getDay();
-var currentHH = todayDate.getHours();
-var openingHH;
-var closingHH;
-var todayDay;
+  var todayDate = new Date();
+  var today = todayDate.getDay();
+  var currentHH = todayDate.getHours();
+  var openingHH;
+  var closingHH;
+  var todayDay;
 
-//Array of weekday values as displayed in HTML
-var weekDays = [1,2,3,4,5,6,0]; //Monday to Sunday
-//If today = to Value of <th> Day
+  //Array of weekday values as displayed in HTML
+  var weekDays = [1,2,3,4,5,6,0]; //Monday to Sunday
+  //If today = to Value of <th> Day
 
-var rowIndex = weekDays.indexOf(today)
+  var rowIndex = weekDays.indexOf(today)
 
-//Target today's table row in each locations table pageReady
-var location1Schedule = document.querySelectorAll('#location-1 tr');
-var location2Schedule = document.querySelectorAll('#location-2 tr');
+  //Target today's table row in each locations table pageReady
+  var location1Schedule = document.querySelectorAll('#location-1 tr');
+  var location2Schedule = document.querySelectorAll('#location-2 tr');
 
 
 
-function openToday(todayIndex, schedule) {
-  var todaySchedule = schedule[todayIndex];
-  //Make Today's schedule bold text make Red
-  todaySchedule.setAttribute("style", "font-size: 1.1em; font-weight: 700; color: #ae3721;")
+  function openToday(todayIndex, schedule) {
+    var todaySchedule = schedule[todayIndex];
+    //Make Today's schedule bold text make Red
+    todaySchedule.setAttribute("style", "font-size: 1.1em; font-weight: 700; color: #ae3721;")
 
-  //Set openingHH for specific Location
-  openingHH = setOpenHH(todayIndex);
-  //Set closingHH for Specific locations
-  closingHH = setCloseHH(todayIndex);
+    //Set openingHH for specific Location
+    openingHH = setOpenHH(todayIndex);
+    //Set closingHH for Specific locations
+    closingHH = setCloseHH(todayIndex);
 
-  //If current Hour is > Opening time
-  if (currentHH >= openingHH && currentHH <= closingHH) {
-  // console.log("We're open!");
-  //Change Opening time cell Data to Open
-  console.log(todaySchedule.children)
-  todaySchedule.children[1].textContent = "OPEN";
-  todaySchedule.children[1].style.textAlign = "right";
-  //Change "to" cell data to "until"
-  todaySchedule.children[2].textContent = " until ";
-  } else {
-  todaySchedule.children[0].textContent = "TODAY";
+    //If current Hour is > Opening time
+    if (currentHH >= openingHH && currentHH <= closingHH) {
+    //Change Opening time cell Data to Open
+    todaySchedule.children[1].textContent = "OPEN";
+    todaySchedule.children[1].style.textAlign = "right";
+    //Change "to" cell data to "until"
+    todaySchedule.children[2].textContent = " until ";
+    } else {
+    todaySchedule.children[0].textContent = "TODAY";
+    }
   }
-}
 
-function setOpenHH(indexOfToday) {
-  //Set openingHH for Location1
-  if (indexOfToday == 1 || indexOfToday == 3) {
-    return 11;
-  } else {
-    return 9;
+  function setOpenHH(indexOfToday) {
+    //Set openingHH for Location1
+    if (indexOfToday == 1 || indexOfToday == 3) {
+      return 11;
+    } else {
+      return 9;
+    }
   }
-}
 
-function setCloseHH(indexOfToday) {
-  //Set openingHH for Location1
-  if (indexOfToday == 6 || indexOfToday == 0) {
-    return 22;
-  } else {
-    return 21;
+  function setCloseHH(indexOfToday) {
+    //Set openingHH for Location1
+    if (indexOfToday == 6 || indexOfToday == 0) {
+      return 22;
+    } else {
+      return 21;
+    }
   }
-}
 
-openToday(rowIndex, location1Schedule);
-openToday(rowIndex, location2Schedule);
+  openToday(rowIndex, location1Schedule);
+  openToday(rowIndex, location2Schedule);
 
 }//end of pageReady
