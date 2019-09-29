6a6b84f630dc65a8336734ff1f44423a560c2750
Paul O <info@paulooidesign.com>
2019-09-29 13:55:02 -0400

    add Header Vue component and public index.html

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HeaderNav company="Seasoning Shack"/>
+  </div>
+</template>
+
+<script>
+import HeaderNav from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HeaderNav
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="large">
+      <MainNav></MainNav>
+      <div id="company-branding"><a href="index.html"><img  src="Logo/logo.svg" id="logo" alt="logo"/></a><h2><a href="index.html">{{company}}</a></h2></div>
+    </header>
+</template>
+
+<script>
+import MainNav from './Navigation.vue';
+
+export default {
+  name: 'HeaderNav',
+  props: {
+    company: String
+  },
+  components: {
+    MainNav
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
